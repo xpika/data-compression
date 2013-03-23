de3a2b880e1e7aae4f5eaea874174dc8d038ab0a
alan hawkins <alanhawkins@alan.local>
2013-03-23 20:05:27 +1100

    rearrange

--- lzw.hs
+++ lzw.hs
@@ -15,22 +15,39 @@ zipit :: (String -> String)
 zipit = viaNum zipit'
 zipit' [] = []
 zipit' (x:xs) = zipit'' (Map.fromList (zip (Data.List.map ((:[]) ) [0..lengthOfKeys]) [0..])) x xs
---zipit' :: [[Char]] -> Char -> [Char] -> [Char]
 zipit'' library buffer [] = buffer : []
-zipit'' library buffer (x:xs) = translated $ zipit'' updatedLibrary updateBuffer xs
-  where (translated,updateBuffer,updatedLibrary) = case Map.lookup [buffer,x] library of
-                                                     Just n -> (id,n,library)
-                                                     _ -> ( (:) buffer
-                                                           ,x
-                                                           ,Map.insert ([buffer,x]) (Map.size library) library 
-                                                          )
+zipit'' library buffer (x:xs) = let key = [buffer,x]
+                                    ( output
+                                     ,buffer'
+                                     ,library'
+                                     ) = case Map.lookup key library of
+                                               Just n -> (  id 
+                                                           ,n 
+                                                           ,library
+                                                         )
+                                               _ -> (  (:) buffer
+                                                      ,x
+                                                      ,Map.insert key (Map.size library) library 
+                                                    )
+                                in output $ zipit'' library' buffer' xs
+
 
 unzipit :: (String -> String)
 unzipit = viaNum unzipit'
 unzipit' [] = []
 unzipit' (x:xs) = unzipit'' (Map.fromList (zip (Data.List.map ((:[]) ) [0..lengthOfKeys]) [0..])) x xs
-unzipit'' library buffer [] = ( (Map.!>) library (buffer)) ++ []
-unzipit'' library buffer (x:xs) = translated $ unzipit'' updatedLibrary updateBuffer xs
-  where (translated,updateBuffer,updatedLibrary) = case Map.lookup [buffer,x] (library) of
-                                                     Just n -> (id,n,library)
-                                                     _ -> ( (++) ( library Map.!> buffer),x,Map.insert (( library Map.!> buffer)++(take 1 $ library Map.!> x )) (Map.size library) library )
+unzipit'' library buffer [] =  library Map.!> buffer
+unzipit'' library buffer (x:xs) = let key = library Map.!> buffer
+                                      (  output
+                                        ,buffer'
+                                        ,library'
+                                       ) = case Map.lookup [buffer,x] library of
+                                                     Just n -> ( id
+                                                                ,n
+                                                                ,library
+                                                                )
+                                                     _ -> (  (++) key
+                                                           , x
+                                                            , Map.insert (key ++ library Map.!> x) (Map.size library) library 
+                                                           )
+                                  in output $ unzipit'' library' buffer' xs
