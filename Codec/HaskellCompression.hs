3cb3f66ed776467b6c67e2faf7c853286111231f
Alan Hawkins <hawk.alan@gmail.com>
2013-12-28 16:30:25 +1100

    added source

---
+++ Codec/HaskellCompression.hs
@@ -0,0 +1,56 @@
+module Codec.HaskellCompression 
+where 
+import Data.List
+import qualified Data.Bimap as Map
+
+import qualified Data.ByteString as B (pack,unpack,ByteString)
+
+
+viaNum f d = B.pack ( map fromIntegral (f (map fromIntegral (B.unpack d))))
+
+lengthOfKeys = 127
+
+zipit :: B.ByteString -> B.ByteString
+zipit = viaNum zipit'
+zipit' [] = []
+zipit' (x:xs) = zipit'' (Map.fromList (zip (Data.List.map ((:[]) ) [0..lengthOfKeys]) [0..])) x xs
+zipit'' library buffer [] = buffer : []
+zipit'' library buffer (x:xs) = let key = [buffer,x]
+                                    ( output
+                                     ,buffer'
+                                     ,library'
+                                     ) = case Map.lookup key library of
+                                               Just n -> (  id 
+                                                           ,n 
+                                                           ,library
+                                                         )
+                                               _ -> (  (:) buffer
+                                                      ,x
+                                                      ,Map.insert key (Map.size library) library 
+                                                    )
+                                in output $ zipit'' library' buffer' xs
+
+
+
+unzipit :: B.ByteString-> B.ByteString                            
+unzipit = viaNum unzipit'
+unzipit' [] = []
+unzipit' (x:xs) = unzipit'' (Map.fromList (zip (Data.List.map ((:[]) ) [0..lengthOfKeys]) [0..])) x xs
+unzipit'' library buffer [] =  library Map.!> buffer
+unzipit'' library buffer (x:xs) = let Just key = buffer `Map.lookupR` library 
+                                      librarySize = Map.size library 
+                                      (  output
+                                        ,buffer'
+                                        ,library'
+                                       ) = case Map.lookup [buffer,x] library of
+                                                     Just n -> ( id
+                                                                ,n
+                                                                ,library
+                                                                )
+                                                     _ -> (  (++) key
+                                                            , x
+                                                            , let Just key' | x == librarySize = Just key
+                                                                            | otherwise =  x `Map.lookupR` library  
+                                                              in Map.insert (key ++ (take 1 key')) librarySize library 
+                                                           )
+                                  in output $ unzipit'' library' buffer' xs
