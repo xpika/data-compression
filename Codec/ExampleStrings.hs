8511c09d0149f9f8f310a6c3d4f5162f11100447
alan hawkins <hawk.alan@gmail.com>
2014-06-15 21:28:04 +1000

    split example strings

---
+++ Codec/ExampleStrings.hs
@@ -0,0 +1,10 @@
+module Codec.ExampleStrings where
+
+examples = [
+   "TOBEORNOTTOBEORTOBEORNOT" 
+  ,"aaaaaa"
+  ,"abababab"
+  ,"abcabcabcabcabcabc"
+  ,"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam dictum nisi et ipsum ornare fermentum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas nullam. "
+  ]
+
--- Tests.hs
+++ Tests.hs
@@ -2,20 +2,13 @@ module Main where
 
 import Codec.HaskellCompression
 import qualified Data.ByteString.Char8 as B
+import Codec.ExampleStrings 
 
 zipitString :: [Char] -> B.ByteString
 zipitString  = zipit  . B.pack
 
 unzipitString :: B.ByteString -> [Char]
 unzipitString  = B.unpack . unzipit
-
-examples = [
-   "TOBEORNOTTOBEORTOBEORNOT" 
-  ,"aaaaaa"
-  ,"abababab"
-  ,"abcabcabcabcabcabc"
-  ,"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam dictum nisi et ipsum ornare fermentum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas nullam. "
-  ]
 
 example str = do 
               print str
@@ -27,5 +20,4 @@ example str = do
             
 main = do
        mapM_ example examples
-
 
